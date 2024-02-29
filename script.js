763c17b580997021db9fc6266aa094ad1b3055dd
yaswanthpalloli3125 <palloliyaswanth@gmail.com>
2024-02-29 23:44:00 +0530

    social media

---
+++ script.js
@@ -0,0 +1,30 @@
+
+let settingsmenu = document.querySelector("#settings");
+let darknbtn = document.querySelector("#dark-btn");
+
+function settingsMenuToggle(){
+    
+    settingsmenu.classList.toggle("settings-menu-height");
+}
+darknbtn.onclick = function(){
+    darknbtn.classList.toggle("dark-btn-on");
+    document.body.classList.toggle("dark-theme");
+
+    if(localStorage.getItem("theme") == "light"){
+        localStorage.setItem("theme", "dark");
+    }else{
+        localStorage.setItem("theme","light");
+    }
+}
+
+if(localStorage.getItem("theme") == "light"){
+    darknbtn.classList.remove("dark-btn-on");
+    document.body.classList.remove("dark-theme");
+
+}else if(localStorage.getItem("theme") == "dark"){
+    darknbtn.classList.add("dark-btn-on");
+    document.body.classList.add("dark-theme");
+
+}else{
+    localStorage.setItem("theme", "light");
+}
